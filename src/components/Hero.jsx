6ff85740688a783c20db8bca2f85f876d4dcd3bc
Froxzen <harvardchong1@gmail.com>
2025-07-03 15:57:21 +1000

    Removed border from hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -30,7 +30,7 @@ const Hero = () => {
 					initial={{ y: 50, opacity: 0 }}
 					animate={{ y: 0, opacity: 1 }}
 					transition={{ duration: 0.8, delay: 0.2 }}
-					className="flex max-w-[768px] w-full flex-col items-center justify-center gap-3 border-red-600 border-2
+					className="flex max-w-[768px] w-full flex-col items-center justify-center gap-3
                 text-center"
 				>
 					<h1
